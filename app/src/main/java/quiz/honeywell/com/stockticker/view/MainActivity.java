e119abcef65381e6a790c36bc48bbaf51cd218b3
rupak <rkg823@gmail.com>
2019-05-04 15:28:45 +0530

    Merge branch 'development' of https://github.com/stockticker-team/StockTicker into development
    # Conflicts:
    #	app/src/main/java/quiz/honeywell/com/stockticker/view/MainActivity.java

--- app/src/main/java/quiz/honeywell/com/stockticker/view/MainActivity.java
+++ app/src/main/java/quiz/honeywell/com/stockticker/view/MainActivity.java
@@ -4,6 +4,9 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
 
 import quiz.honeywell.com.stockticker.R;
 import quiz.honeywell.com.stockticker.databinding.ActivityMainBinding;
@@ -11,10 +14,26 @@ import quiz.honeywell.com.stockticker.viewmodel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    AutoCompleteTextView autoCompleteTextView;
     private MainActivityViewModel mainDO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        ArrayList<String> searchArrayList= new ArrayList<String>();
+
+        searchArrayList.add("Facebook");
+        searchArrayList.add("Amazon");
+        searchArrayList.add("MicroSoft");
+        searchArrayList.add("HoneyWell");
+        searchArrayList.add("Infosys");
+        searchArrayList.add("Tata Services");
+        searchArrayList.add("Qualcomm");
+//initilaze this array with your data
+        AutoCompleteAdapter adapter = new AutoCompleteAdapter(this, android.R.layout.simple_dropdown_item_1line, android.R.id.text1, searchArrayList);
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.search);
+        autoCompleteTextView.setAdapter(adapter);
 
 
     }
