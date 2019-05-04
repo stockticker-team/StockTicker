5aa7e6ad88c08fb5ba95147c302091173747706e
tsaikiran530 <tsaikiran.info@gmail.com>
2019-05-04 14:40:11 +0530

    details activity

--- app/src/main/java/quiz/honeywell/com/stockticker/view/DeatilsActivity.java
+++ app/src/main/java/quiz/honeywell/com/stockticker/view/DeatilsActivity.java
@@ -2,14 +2,30 @@ package quiz.honeywell.com.stockticker.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import quiz.honeywell.com.stockticker.R;
 
 public class DeatilsActivity extends AppCompatActivity {
 
+    private TextView tvName,tvValue,tvValue1,tvValue2,tvType,tvClose,tvSymbol,tvRangeVal,tvOpenVal,tvMarketVal,tvPrevVal,tvRatioVal;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deatils);
+
+        tvName = findViewById(R.id.tvName);
+        tvValue = findViewById(R.id.tvValue);
+        tvValue1 = findViewById(R.id.tvValue1);
+        tvValue2 = findViewById(R.id.tvValue2);
+        tvType = findViewById(R.id.tvType);
+        tvClose = findViewById(R.id.tvClose);
+        tvSymbol = findViewById(R.id.tvSymbol);
+        tvRangeVal = findViewById(R.id.tvRangeVal);
+        tvOpenVal = findViewById(R.id.tvOpenVal);
+        tvMarketVal = findViewById(R.id.tvMarketVal);
+        tvPrevVal = findViewById(R.id.tvPrevVal);
+        tvRatioVal = findViewById(R.id.tvRatioVal);
     }
 }
