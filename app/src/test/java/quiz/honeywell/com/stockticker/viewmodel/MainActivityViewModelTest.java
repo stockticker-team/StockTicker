10f94eff81ddbac43bc010af7e2de9a524e23d59
Sagar <sagarchalavadi@gmail.com>
2019-05-04 14:06:37 +0530

    Deepika-ut

---
+++ app/src/test/java/quiz/honeywell/com/stockticker/viewmodel/MainActivityViewModelTest.java
@@ -0,0 +1,11 @@
+package quiz.honeywell.com.stockticker.viewmodel;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by ADMIN on 5/4/2019.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MainActivityViewModelTest {
+}
